6bc21d76d09005bd4747384a0d330d2c164f7ec2
Shouvik Paul <shouvik.paul@ah.nl>
2024-09-23 17:47:57 +0200

    initial commit

---
+++ src/test/java/org/shvk/cloudgateway/CloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.shvk.cloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
