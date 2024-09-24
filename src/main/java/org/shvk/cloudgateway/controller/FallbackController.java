c92a267f7e68d2052c7d161a1e7fb31aa269ee56
Shouvik Paul <shouvik.paul@ah.nl>
2024-09-24 12:50:23 +0200

    adding fallback controller

---
+++ src/main/java/org/shvk/cloudgateway/controller/FallbackController.java
@@ -0,0 +1,23 @@
+package org.shvk.cloudgateway.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FallbackController {
+
+    @GetMapping("/orderServiceFallback")
+    public String orderServiceFallback() {
+        return "Order Service is Down!";
+    }
+
+    @GetMapping("/paymentServiceFallback")
+    public String paymentServiceFallback() {
+        return "Payment Service is Down!";
+    }
+
+    @GetMapping("/productCatalogServiceFallback")
+    public String productCatalogServiceFallback() {
+        return "Product Catalog Service is Down!";
+    }
+}
