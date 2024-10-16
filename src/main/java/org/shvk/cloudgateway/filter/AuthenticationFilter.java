67c570b0d09ee0267acc7e4933bf9b4a6b548716
Shouvik Paul <shouvik.paul@ah.nl>
2024-10-16 07:58:08 +0200

    added header in underline api calls

--- src/main/java/org/shvk/cloudgateway/filter/AuthenticationFilter.java
+++ src/main/java/org/shvk/cloudgateway/filter/AuthenticationFilter.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -23,6 +24,7 @@ public class AuthenticationFilter extends AbstractGatewayFilterFactory<Authentic
     @Override
     public GatewayFilter apply(Config config) {
         return ((exchange, chain) -> {
+            ServerHttpRequest request = null;
             if (validator.isSecured.test(exchange.getRequest())) {
                 //header contains token or not
                 if (!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
@@ -36,13 +38,17 @@ public class AuthenticationFilter extends AbstractGatewayFilterFactory<Authentic
                 try {
                     //REST call to AUTH service "http://IDENTITY-SERVICE//validate?token=authHeader");
                     jwtUtil.validateToken(authHeader);
+                    request = exchange.getRequest()
+                        .mutate()
+                        .header("loggedInUser", jwtUtil.extractUserName(authHeader))
+                        .build();
 
                 } catch (Exception e) {
                     System.out.println("invalid access...!");
                     throw new RuntimeException("un authorized access to application");
                 }
             }
-            return chain.filter(exchange);
+            return chain.filter(exchange.mutate().request(request).build());
         });
     }
 
--- src/main/java/org/shvk/cloudgateway/util/JwtUtil.java
+++ src/main/java/org/shvk/cloudgateway/util/JwtUtil.java
@@ -1,11 +1,13 @@
 package org.shvk.cloudgateway.util;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
+import java.util.function.Function;
 
 @Component
 public class JwtUtil {
@@ -19,5 +21,23 @@ public class JwtUtil {
     private Key getSignKey() {
         byte[] keyBytes = Decoders.BASE64.decode(SECRET);
         return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String extractUserName(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+            .parserBuilder()
+            .setSigningKey(getSignKey())
+            .build()
+            .parseClaimsJws(token)
+            .getBody();
     }
 }
